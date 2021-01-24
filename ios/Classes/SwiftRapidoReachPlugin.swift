9d29be21d168c4d74081b1b44be6ffc9f39256b7
Vikash Kumar <kvikash766@gmail.com>
2021-01-24 16:50:19 +0530

    ios sdk intergration complete

--- ios/Classes/SwiftRapidoReachPlugin.swift
+++ ios/Classes/SwiftRapidoReachPlugin.swift
@@ -5,80 +5,66 @@ import UIKit
 @objc(SwiftRapidoReachPlugin)
 public class SwiftRapidoReachPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "RapidoReach", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "rapidoreach", binaryMessenger: registrar.messenger())
     let instance = SwiftRapidoReachPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
-    channel.setMethodCallHandler {(call: FlutterMethodCall, result: FlutterResult) -> Void in
-            if (call.method == "init") {
+  }
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+            if call.method == "init" {
+                let apiKey = (call.arguments as! Dictionary<String, AnyObject>)["api_token"] as! String
+                let userId = (call.arguments as! Dictionary<String, AnyObject>)["user_id"] as! String
                 RapidoReach.shared.setRewardCallback { (reward:Int) in
-                  print("%d REWARD", reward);
-//                    try? self.onReward(reward: reward)
+                  print("%d REWARD", reward, apiKey, userId);
+                  self.onReward(reward: reward)
                 }
                 RapidoReach.shared.setsurveysAvailableCallback { (available:Bool) in
                   print("Rapido Reach Survey Available" );
-            //      RNRapidoReach.EventEmitter.sendEvent(withName: "onRewardCenterClosed", body: nil)
-//                  self.rapidoreachSurveyAvailable(available: available)
+                  self.rapidoreachSurveyAvailable(available: available)
                 }
                 RapidoReach.shared.setrewardCenterOpenedCallback {
                   print("Reward centre opened")
-//                  self.onRewardCenterOpened()
+                  self.onRewardCenterOpened()
                 }
                 RapidoReach.shared.setrewardCenterClosedCallback {
                   print("Reward centre closed" );
-            //      RNRapidoReach.EventEmitter.sendEvent(withName: "onRewardCenterClosed", body: nil)
-//                  self.onRewardCenterClosed()
-
-
+//               RNRapidoReach.EventEmitter.sendEvent(withName: "onRewardCenterClosed", body: nil)
+                  self.onRewardCenterClosed()
                 }
-//                RapidoReach.shared.configure(apiKey: apiKey as String, user: userId as String)
-//                RapidoReach.shared.fetchAppUserID()
+                RapidoReach.shared.configure(apiKey: apiKey as String, user: userId as String)
+                RapidoReach.shared.fetchAppUserID()
+                result(nil)
+            } else if (call.method == "show") {
+                // we do not keep track of old callbacks on iOS, so nothing to do here
+                let iframeController = topMostController()
+                if(iframeController == nil) {
+                  return
+                }
+                RapidoReach.shared.presentSurvey(iframeController!)
+                result(nil)
+            } else if (call.method == "onRewardCenterOpened") {
+                print("Native test  RewardCenterClosed");
+                result(0)
+            } else if (call.method == "onReward") {
+                let quantity = call.arguments
+                result(quantity)
+            } else if (call.method == "onRewardCenterClosed") {
+                print("Native test  RewardCenterOpened");
+                result(0)
+            } else if (call.method == "rapidoReachSurveyAvailable") {
+                let surveyAvailable = call.arguments
+                var survey = 0;
+                if((surveyAvailable) != nil) {
+                   survey = 1;
+                } else if ((surveyAvailable == nil)) {
+                  survey = 0;
+               }
+                result(survey)
+            }
+            else {
+                result(FlutterMethodNotImplemented)
             }
         }
-    // Override point for customization after application launch.
-    
-  }
-    
-    
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
-    
-//    static func moduleName() -> String!{
-//      return "RNRapidoReach";
-//    }
-//
-//    static func requiresMainQueueSetup () -> Bool {
-//      return true;
-//    }
-    
-
-    public func initWithApiKeyAndUserId(_ call: FlutterMethodCall, result: @escaping FlutterResult, _ apiKey:NSString, userId:NSString) -> Void {
-    // Override point for customization after application launch.
-      RapidoReach.shared.setRewardCallback { (reward:Int) in
-        print("%d REWARD", reward);
-        self.onReward(reward: reward)
-      }
-      RapidoReach.shared.setsurveysAvailableCallback { (available:Bool) in
-        print("Rapido Reach Survey Available" );
-  //      RNRapidoReach.EventEmitter.sendEvent(withName: "onRewardCenterClosed", body: nil)
-        self.rapidoreachSurveyAvailable(available: available)
-      }
-      RapidoReach.shared.setrewardCenterOpenedCallback {
-        print("Reward centre opened")
-        self.onRewardCenterOpened()
-      }
-      RapidoReach.shared.setrewardCenterClosedCallback {
-        print("Reward centre closed" );
-  //      RNRapidoReach.EventEmitter.sendEvent(withName: "onRewardCenterClosed", body: nil)
-        self.onRewardCenterClosed()
-
-
-      }
-      RapidoReach.shared.configure(apiKey: apiKey as String, user: userId as String)
-      RapidoReach.shared.fetchAppUserID()
-        //    return true
-    }
 
     func topMostController() -> UIViewController? {
         guard let window = UIApplication.shared.keyWindow, let rootViewController = window.rootViewController else {
@@ -95,41 +81,29 @@ public class SwiftRapidoReachPlugin: NSObject, FlutterPlugin {
     }
 
     public func showRewardCenter(_ call: FlutterMethodCall, result: @escaping FlutterResult) -> Void {
-      let iframeController = topMostController()
-      if(iframeController == nil) {
-        return
-      }
-        RapidoReach.shared.presentSurvey(iframeController!)
+      
       
     }
 
     public func onReward(reward: Int) {
-          // send our event with some data
-//          sendEvent(withName: "onReward", body: reward)
-          // body can be anything: int, string, array, object
     }
 
     public func onRewardCenterOpened()  {
       print("Native test  RewardCenterOpened");
-//      sendEvent(withName: "onRewardCenterOpened", body: "opened")
     }
 
     public func onRewardCenterClosed() {
       print("Native test  RewardCenterClosed");
-//      sendEvent(withName: "onRewardCenterClosed", body: "reward")
     }
 
 
     func rapidoreachSurveyAvailable(available: Bool)  {
         print("Native test  rapidoreachSurveyAvailable");
-//       sendEvent(withName: "rapidoreachSurveyAvailable", body: "true")
     }
-  
   
      func supportedEvents() -> [String]! {
         return ["onReward", "onRewardCenterOpened", "onRewardCenterClosed", "rapidoreachSurveyAvailable"]
       }
-    
 }
 
 
